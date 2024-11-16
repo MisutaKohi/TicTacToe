f2785ee347b8a7be974b7d71f35194362c9dbe87
Ryan Morgan <RyanOnGithub@gmail.com>
2024-11-16 11:11:37 -0800

    Move player constructor to its own file

---
+++ player.js
@@ -0,0 +1,38 @@
+function createPlayer(char) {
+  const symbol = char;
+  let hasWon = false;
+
+  const rows = new Array(3).fill(0);
+  const cols = new Array(3).fill(0);
+  let diagonalForward = 0;
+  let diagonalBackward = 0;
+  
+  function move(row, col) {
+    rows[row]++;
+    cols[col]++;
+    
+    if (row == col) {
+      diagonalBackward++;
+    } 
+    
+    if (row + col === 2) {
+      diagonalForward++;
+    }
+
+    hasWon = rows[row] === 3 || 
+      cols[col] === 3 ||
+      diagonalBackward === 3 ||
+      diagonalForward === 3;
+  }
+
+  function hasPlayerWon() {
+    return hasWon;
+  }
+
+  return {
+    move,
+    hasPlayerWon
+  }
+}
+
+export default createPlayer;
